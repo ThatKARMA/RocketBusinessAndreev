53371e55d32ece488da6e0747881c154b3f9b9a0
ThatKARMA <reeper98@mail.ru>
2023-09-13 14:03:25 +0300

    add slider

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-$(document).ready(function () {
-	$('.carousel__inner').slick({
-			infinite: true,
-			speed: 500,
-			prevArrow: '<button type="button" class="slick-prev"><img src="build/img/arrow-prev.png"></button>',
-			nextArrow: '<button type="button" class="slick-next"><img src="build/img/arrow-next.png"></button>',
-	});
-});
+$('.multiple-items').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3
+  });
